c1502716502db22fad4375384a10d7fe8b06b1a0
Paolo Caramia <caramia.paolo1984@gmail.com>
2024-08-25 16:52:07 +0200

    risoluzione bug

--- projects/netflix-react/src/App.jsx
+++ projects/netflix-react/src/App.jsx
@@ -1,85 +1,85 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Navigation from './components/Navigation/Navigation'
-import MovieList from './components/MovieList/MovieList'
-import { AUTH_KEY } from './constants'
-import MovieDetail from './components/MovieDetail/MovieDetail'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import MovieList from "./components/MovieList/MovieList";
+import { AUTH_KEY } from "./constants";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 
 const items = [
   {
     id: 0,
-    text: 'Home',
-    href: '/#',
+    text: "Home",
+    href: "/#",
   },
   {
     id: 1,
-    text: 'TV Shows',
-    href: '/#',
+    text: "TV Shows",
+    href: "/#",
   },
   {
     id: 2,
-    text: 'Movies',
-    href: '/#',
+    text: "Movies",
+    href: "/#",
   },
   {
     id: 3,
-    text: 'New & Popular',
-    href: '/#',
+    text: "New & Popular",
+    href: "/#",
   },
-]
+];
 
-const BASE_URL = 'https://api.themoviedb.org/3/movie/'
+const BASE_URL = "https://api.themoviedb.org/3/movie/";
 
 function App() {
-  const [popularList, setPopularList] = useState([])
-  const [topRatedList, setTopRatedList] = useState([])
-  const [movieDetail, setMovieDetail] = useState(null)
+  const [popularList, setPopularList] = useState([]);
+  const [topRatedList, setTopRatedList] = useState([]);
+  const [movieDetail, setMovieDetail] = useState(null);
 
   useEffect(() => {
     fetch(`${BASE_URL}popular`, {
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization: `Bearer ${AUTH_KEY}`,
       },
     })
       .then((res) => res.json())
-      .then((data) => setPopularList(data.results))
+      .then((data) => setPopularList(data.results));
 
     fetch(`${BASE_URL}top_rated`, {
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization: `Bearer ${AUTH_KEY}`,
       },
     })
       .then((res) => res.json())
-      .then((data) => setTopRatedList(data.results))
+      .then((data) => setTopRatedList(data.results));
 
     fetch(`${BASE_URL}429`, {
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization: `Bearer ${AUTH_KEY}`,
       },
     })
       .then((response) => response.json())
-      .then((data) => setMovieDetail(data))
-  }, [])
+      .then((data) => setMovieDetail(data));
+  }, []);
 
   return (
     <div>
-      {/* <Navigation items={items} />
+      <Navigation items={items} />
       <MovieList movieList={popularList}>
         <h1>My Fav Movies</h1>
         <p>Things you like the most</p>
       </MovieList>
       <MovieList movieList={topRatedList}>
         <h3>Top Rated</h3>
-      </MovieList> */}
+      </MovieList>
       {movieDetail && (
         <MovieDetail
           cast={[
-            { id: 0, name: 'Clint Eastwood' },
-            { id: 1, name: 'Eli Wallach' },
-            { id: 2, name: 'Lee Van Cleef' },
+            { id: 0, name: "Clint Eastwood" },
+            { id: 1, name: "Eli Wallach" },
+            { id: 2, name: "Lee Van Cleef" },
           ]}
           genres={movieDetail.genres}
           image={`https://image.tmdb.org/t/p/w500${movieDetail.backdrop_path}`}
@@ -90,7 +90,7 @@ function App() {
         />
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
